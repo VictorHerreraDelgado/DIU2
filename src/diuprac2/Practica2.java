6cbe424b30befd03743f2f313e7938c74c0ae5a1
MiguelHerreraAlv <michha02@gmail.com>
2019-09-21 21:31:10 +0100

    Final version 1.0

--- src/diuprac2/Practica2.java
+++ src/diuprac2/Practica2.java
@@ -14,8 +14,7 @@ import java.util.Random;
  * @author Asus
  */
 public class Practica2 extends javax.swing.JFrame {
-    private final int [][] mat = new int[10][10];
-    private int minValue, maxValue;
+    public int [][] mat = new int[10][10];
     /**
      * Creates new form Practica2
      */
@@ -43,6 +42,8 @@ public class Practica2 extends javax.swing.JFrame {
         resPanel = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         areaTexto = new javax.swing.JTextArea();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,9 +52,6 @@ public class Practica2 extends javax.swing.JFrame {
         minVText.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 minVTextFocusGained(evt);
-            }
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                minVTextFocusLost(evt);
             }
         });
         minVText.addActionListener(new java.awt.event.ActionListener() {
@@ -65,9 +63,6 @@ public class Practica2 extends javax.swing.JFrame {
         maxVText.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 maxVTextFocusGained(evt);
-            }
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                maxVTextFocusLost(evt);
             }
         });
         maxVText.addActionListener(new java.awt.event.ActionListener() {
@@ -83,11 +78,6 @@ public class Practica2 extends javax.swing.JFrame {
         barraDes.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 barraDesStateChanged(evt);
-            }
-        });
-        barraDes.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                barraDesFocusGained(evt);
             }
         });
 
@@ -138,15 +128,19 @@ public class Practica2 extends javax.swing.JFrame {
         resPanel.setLayout(resPanelLayout);
         resPanelLayout.setHorizontalGroup(
             resPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 872, Short.MAX_VALUE)
         );
         resPanelLayout.setVerticalGroup(
             resPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(resPanelLayout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 366, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(53, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        jLabel4.setText("Miguel Herrera Álvarez");
+
+        jLabel3.setText("Víctor Herrera Delgado");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -158,6 +152,12 @@ public class Practica2 extends javax.swing.JFrame {
                     .addComponent(conPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(resPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(325, 325, 325)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -165,7 +165,12 @@ public class Practica2 extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(conPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
-                .addComponent(resPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(resPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(22, 22, 22))
         );
 
         pack();
@@ -173,20 +178,20 @@ public class Practica2 extends javax.swing.JFrame {
 
     private void minVTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minVTextActionPerformed
         // TODO add your handling code here:
-        areaTexto.requestFocus();
+        barraDes.requestFocus();
         recreaMatriz();
-        actualizaMatriz();
     }//GEN-LAST:event_minVTextActionPerformed
 
     private void maxVTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_maxVTextActionPerformed
         // TODO add your handling code here:
-        areaTexto.requestFocus();
-        recreaMatriz();   
+        barraDes.requestFocus();
+        recreaMatriz();
+            
     }//GEN-LAST:event_maxVTextActionPerformed
 
     private void barraDesStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_barraDesStateChanged
         // TODO add your handling code here:
-        if (maxValue > minValue){
+        if (valida()){
             actualizaMatriz();
         }
     }//GEN-LAST:event_barraDesStateChanged
@@ -200,25 +205,6 @@ public class Practica2 extends javax.swing.JFrame {
         // TODO add your handling code here:
         maxVText.setForeground(Color.black);
     }//GEN-LAST:event_maxVTextFocusGained
-
-    private void minVTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_minVTextFocusLost
-        // TODO add your handling code here:
-        if (valida(minVText.getText())){
-            minValue = parseInt(minVText.getText());
-        }
-    }//GEN-LAST:event_minVTextFocusLost
-
-    private void maxVTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_maxVTextFocusLost
-        // TODO add your handling code here:
-        if (valida(maxVText.getText())){
-            maxValue = parseInt(minVText.getText());
-        }
-    }//GEN-LAST:event_maxVTextFocusLost
-
-    private void barraDesFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_barraDesFocusGained
-        // TODO add your handling code here:
-        recreaMatriz();
-    }//GEN-LAST:event_barraDesFocusGained
     
     /**
      * @param args the command line arguments
@@ -261,6 +247,8 @@ public class Practica2 extends javax.swing.JFrame {
     private javax.swing.JPanel conPanel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField maxVText;
     private javax.swing.JTextField minVText;
@@ -280,53 +268,56 @@ public class Practica2 extends javax.swing.JFrame {
                 }
             }
             texto += "\n\n";
-       }
-       areaTexto.setText(texto);
-       System.out.println("Valor de la barra: " + barValue);
-     
+        }
+        areaTexto.setText(texto);
+        
+        if(barraDes.getMinimum() == Integer.parseInt(minVText.getText()) && 
+            barraDes.getMaximum() == Integer.parseInt(maxVText.getText())){
+            System.out.println("Valor de la barra: " + barraDes.getValue());
+        }
     }
 
     private void recreaMatriz() {
-        if (valida(minVText.getText()) && valida(maxVText.getText())){
-            minValue = parseInt(minVText.getText());
-            maxValue = parseInt(maxVText.getText());
-            //COMPROBAR QUE EL MÁXIMO SEA MAYOR QUE EL MÍNIMO
-            if (!(maxValue > minValue)){
-                System.err.println("Introduzca solo valores numéricos y siendo el mínimo menor que el máximo\n");
-                maxVText.setForeground(Color.red);
-                minVText.setForeground(Color.red);
-            } else {
-                Random r = new Random();
-                int result = r.nextInt((int) ((maxValue - minValue) + minValue));
-                //System.out.println("el valor es: "  + (int) ((maxValue-minValue)/10));
-                for (int i = 0 ; i < 10; i++){
-                    for (int j = 0; j < 10; j++){
-                        mat[i][j] = r.nextInt((maxValue - minValue) + 1) + minValue;//(int )(Math.random() * maxValue + minValue);
-                    }
+        if (valida()){
+            maxVText.setForeground(Color.black);
+            minVText.setForeground(Color.black);
+            int minValue = parseInt(minVText.getText());
+            int maxValue = parseInt(maxVText.getText());
+            Random r = new Random();
+            int result = r.nextInt((int) ((maxValue - minValue) + minValue));
+            //System.out.println("el valor es: "  + (int) ((maxValue-minValue)/10));
+            for (int i = 0 ; i < 10; i++){
+                for (int j = 0; j < 10; j++){
+                    mat[i][j] = r.nextInt((maxValue - minValue) + 1) + minValue;//(int )(Math.random() * maxValue + minValue);
                 }
-                barraDes.setLabelTable(null); //Esta funcion impide que el jslider se sobrecargue de labels con diferencias Max-Min muy grandes
-                if ((maxValue - minValue) <= 10){
-                    barraDes.setMajorTickSpacing(1);
-                    barraDes.setMinorTickSpacing(1);
-                }else{
-                    barraDes.setMajorTickSpacing(((maxValue - minValue)/10));
-                    barraDes.setMinorTickSpacing(((maxValue - minValue)/10));
-                }
-                barraDes.setMinimum(minValue);
-                barraDes.setMaximum(maxValue);
-                barraDes.setPaintTicks(true);
-                barraDes.setPaintLabels(true);
-                barraDes.setValue(minValue);
-
-                actualizaMatriz(); 
             }
+            barraDes.setLabelTable(null); //Esta funcion impide que el jslider se sobrecargue de labels con diferencias Max-Min muy grandes
+            if ((maxValue - minValue) <= 10){
+                barraDes.setMajorTickSpacing(1);
+                barraDes.setMinorTickSpacing(1);
+            }else{
+                barraDes.setMajorTickSpacing(((maxValue - minValue)/10));
+                barraDes.setMinorTickSpacing(((maxValue - minValue)/10));
+            }
+            
+            barraDes.setPaintTicks(true);
+            barraDes.setPaintLabels(true);
+            barraDes.setMinimum(minValue);
+            barraDes.setValue(minValue);
+            barraDes.setMaximum(maxValue);
+            
+            
+        } else {
+            barraDes.setPaintTicks(false);
+            barraDes.setPaintLabels(false);
+            areaTexto.setText("");
         }
-  
     }
 
-    private boolean valida(String s) {
+    private boolean valida() {
         String rex = "\\d+";
-        if(!s.matches(rex)){
+        if(!minVText.getText().matches(rex) || 
+                !maxVText.getText().matches(rex)){
             System.err.println("Introduzca solo valores numéricos y siendo el mínimo menor que el máximo\n");
             if (!minVText.getText().matches(rex)){
                 minVText.setForeground(Color.red);
@@ -334,10 +325,16 @@ public class Practica2 extends javax.swing.JFrame {
             if (!maxVText.getText().matches(rex)){
                 maxVText.setForeground(Color.red);
             }
-            
+
             return false;
-        } else {
-            return true;
+        } 
+        //COMPROBAR QUE EL MÁXIMO SEA MAYOR QUE EL MÍNIMO
+        if (Integer.parseInt(maxVText.getText()) <= Integer.parseInt(minVText.getText())){
+            System.err.println("Introduzca solo valores numéricos y siendo el mínimo menor que el máximo\n");
+            maxVText.setForeground(Color.red);
+            minVText.setForeground(Color.red);
+            return false;
         }
+        return true;
     }
 }
